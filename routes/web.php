2ca1adbfbc43013f89cc47c3b5feed31d85c5aa6
Nu'man Nasyar MZ <numannasyarmz11@gmail.com>
2024-05-07 08:33:59 +0700

    Feat: Categories CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\Auth\UserController;
 
 Route::get('/', function () {
@@ -12,4 +13,5 @@ Route::middleware(['auth'])->group(function () {
         return view('pages.dashboard', ["type_menu" => "dashboard"]);
     })->name('home');
     Route::resource('users', UserController::class);
+    Route::resource('categories', CategoryController::class);
 });
